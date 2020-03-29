0374f2bd4b46f83053641924c6e18a82ffc1d040
David Ewert <dewert10@gmail.com>
2020-03-29 16:13:04 -0700

    Tweaked method for getting class info.

--- src/ubc/cs304/team64/model/DatabaseConnectionHandler.java
+++ src/ubc/cs304/team64/model/DatabaseConnectionHandler.java
@@ -145,7 +145,7 @@ public class DatabaseConnectionHandler {
           "WHERE c.time = t.time AND c.rid = t.rid AND c.fid = t.fid) " +
           "as taking " +
           "FROM class c NATURAL JOIN classt " +
-          "WHERE fid = ?");
+          "WHERE fid = ? time > CURRENT_TIMESTAMP() time > CURRENT_TIMESTAMP()");
       ps.setInt(1, facility.getFid());
       Collection<ClassInfo> classes = new ArrayList<>();
       ResultSet rs = ps.executeQuery();
