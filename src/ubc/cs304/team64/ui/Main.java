cf0143653e16b3a3c21d8a80f50587694bd6dc69
Sukh <brownie2011@live.ca>
2020-03-29 22:26:49 -0700

    UI C4

---
+++ src/ubc/cs304/team64/ui/Main.java
@@ -0,0 +1,31 @@
+package ubc.cs304.team64.ui;
+
+import ubc.cs304.team64.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args){
+        System.out.println("Esketit");
+
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
