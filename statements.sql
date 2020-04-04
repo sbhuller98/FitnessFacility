dbc71b31c4d2143a85501d2e8ee89a0755b9032a
David Ewert <dewert10@gmail.com>
2020-04-03 18:48:34 -0700

    Added consolidate file of sql statements.

---
+++ statements.sql
@@ -0,0 +1,23 @@
+PREPARE getMember FROM 'SELECT * FROM member NATURAL JOIN status WHERE login = ? AND password = ?';
+PREPARE getStatuses FROM 'SELECT sType FROM status';
+PREPARE createMember FROM 'INSERT INTO member(login, password, address, phoneNumber, name, birthDate, driverLicenceNumber, sType) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+PREPARE addPayment FROM 'INSERT INTO memberpayment(mid, pid) VALUES (?,?)';
+PREPARE getClassTypesFromStatus FROM 'SELECT classType FROM letsyoutake WHERE sType = ?';
+PREPARE getStatusCost FROM 'SELECT cost FROM status WHERE sType = ?';
+PREPARE createCreditCard FROM 'INSERT INTO creditcard(num, expiryDate, csv, nameOnCard) VALUES (?,?,?,?)';
+PREPARE createPayment FROM 'INSERT INTO payment(frequency, creditCardNumber, accountNumber) VALUES (?, ?, NULL)';
+PREPARE getPayments FROM 'SELECT * FROM payment NATURAL JOIN memberpayment WHERE mid = ?';
+PREPARE getCreditCard FROM 'SELECT * FROM creditcard WHERE num = ?';
+PREPARE getPap FROM 'SELECT * FROM PAPAccount WHERE accountNumber = ?';
+
+PREPARE getFacilities FROM 'SELECT * FROM facility';
+PREPARE getClasses FROM
+    'SELECT *,
+          (SELECT COUNT(t.mid) FROM takes t
+          WHERE c.time = t.time AND c.rid = t.rid AND c.fid = t.fid)
+          as taking
+          FROM instructor i NATURAL JOIN class c NATURAL JOIN classt
+          WHERE fid = ? AND time > CURRENT_TIMESTAMP()';
+PREPARE getInstructor FROM 'SELECT * FROM ratedinstructors i WHERE i.iid = ?';
+PREPARE getInstructorsInFacility FROM 'SELECT i.* FROM ratedinstructors i WHERE
+    i.iid IN (SELECT c.iid FROM class c WHERE c.time > CURRENT_TIMESTAMP AND c.iid = iid AND c.fid = ?)';
