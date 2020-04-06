7e52714f84e2a1f1fc82dc110bc19f6af3f0a349
David Ewert <dewert10@gmail.com>
2020-04-05 21:07:58 -0700

    Added semicolon

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 CREATE DATABASE IF NOT EXISTS Project;
 USE Project;
 CREATE TABLE Facility(
