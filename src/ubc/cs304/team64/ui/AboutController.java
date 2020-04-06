74801515ef29a0325cc17bdebb066b60ae9eecc5
Sukh <brownie2011@live.ca>
2020-04-05 21:26:55 -0700

    minor fixes

--- src/ubc/cs304/team64/ui/AboutController.java
+++ src/ubc/cs304/team64/ui/AboutController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import ubc.cs304.team64.model.Facility;
 import ubc.cs304.team64.model.Member;
 import ubc.cs304.team64.util.FXMLLoaderWrapper;
@@ -31,7 +32,9 @@ public class AboutController implements Initializable {
     public void setUp(Facility facility, Member member) {
         back.setOnAction(e -> FacilityController.setStage(facility, member));
         description.setWrapText(true);
+
         address.setWrapText(true);
+        description.setWrapText(true);
         description.setText(facility.getDescription());
         address.setText("Address: " + facility.getAddress());
         image.setImage(new Image(getClass().getResource("/facilities/image" + facility.getFid() + ".jpg").toString()));
